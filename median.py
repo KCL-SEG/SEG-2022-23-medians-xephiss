76fef9b7ac9de0f4a6cc14d37a5f435d9ce4ee4a
Alex Lam <alex.lam@kcl.ac.uk>
2023-09-29 14:45:55 +0100

    Add medium algorithm to file

--- median.py
+++ median.py
@@ -9,4 +9,11 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+numbers.sort()
+length = len(numbers)
+if length % 2 == 0:
+    mid = (numbers[length//2] + numbers[(length//2)-1])/2
+    print(mid)
+else:
+    print(numbers[length//2])
